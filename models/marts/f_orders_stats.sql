e9c68ff7af66ecdb62042bf23af49838489de8b6
elenasamsonenko <129121912+elenasamsonenko@users.noreply.github.com>
2024-02-13 10:03:28 +0000

    homework

--- models/marts/f_orders_stats.sql
+++ models/marts/f_orders_stats.sql
@@ -1,7 +1,7 @@
 {{
     config (
       engine='MergeTree()',
-      order_by=['O_ORDERDATE']
+      order_by=['O_ORDERYEAR']
     )
 }}
 
@@ -12,7 +12,7 @@ SELECT
     , count(DISTINCT O_ORDERKEY) AS num_orders
     , count(DISTINCT C_CUSTKEY) AS num_customers
     , sum(L_EXTENDEDPRICE * L_DISCOUNT) AS revenue
-FROM {{ ref('f_lineorder_flat') }} AS f
+FROM {{ ref('f_lineorder_flat') }}
 WHERE 1=1
 GROUP BY
     toYear(O_ORDERDATE)
